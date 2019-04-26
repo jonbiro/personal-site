d788b492e8520914140ed1d4ff4744ef851f9715
Jonathan Biro <jonnybrx@gmail.com>
2019-04-26 19:16:53 -0400

    favicon

--- src/sections/Writing.js
+++ src/sections/Writing.js
@@ -10,29 +10,29 @@ import Triangle from '../components/Triangle';
 import ImageSubtitle from '../components/ImageSubtitle';
 
 const Background = () => (
-  <div>
-    <Triangle
-      color="backgroundDark"
-      height={['15vh', '10vh']}
-      width={['100vw', '100vw']}
-      invertX
-    />
-
-    <Triangle
-      color="secondary"
-      height={['50vh', '40vh']}
-      width={['70vw', '40vw']}
-      invertY
-    />
-
-    <Triangle
-      color="primaryDark"
-      height={['40vh', '15vh']}
-      width={['100vw', '100vw']}
-      invertX
-      invertY
-    />
-  </div>
+	<div>
+		<Triangle
+			color="backgroundDark"
+			height={['15vh', '10vh']}
+			width={['100vw', '100vw']}
+			invertX
+		/>
+		
+		<Triangle
+			color="secondary"
+			height={['50vh', '40vh']}
+			width={['70vw', '40vw']}
+			invertY
+		/>
+		
+		<Triangle
+			color="primaryDark"
+			height={['40vh', '15vh']}
+			width={['100vw', '100vw']}
+			invertX
+			invertY
+		/>
+	</div>
 );
 
 const CoverImage = styled.img`
@@ -50,57 +50,57 @@ const EllipsisHeading = styled(Heading)`
 `;
 
 const Post = ({ title, text, image, url, date, time }) => (
-  <Card onClick={() => window.open(url, '_blank')} pb={4}>
-    <EllipsisHeading m={3} p={1}>
-      {title}
-    </EllipsisHeading>
-    {image && <CoverImage src={image} height="200px" alt={title} />}
-    <Text m={3}>{text}</Text>
-    <ImageSubtitle bg="primaryLight" color="white" x="right" y="bottom" round>
-      {`${date} - ${Math.ceil(time)} min`}
-    </ImageSubtitle>
-  </Card>
+	<Card onClick={() => window.open(url, '_blank')} pb={4}>
+		<EllipsisHeading m={3} p={1}>
+			{title}
+		</EllipsisHeading>
+		{image && <CoverImage src={image} height="200px" alt={title} />}
+		<Text m={3}>{text}</Text>
+		<ImageSubtitle bg="primaryLight" color="white" x="right" y="bottom" round>
+			{`${date} - ${Math.ceil(time)} min`}
+		</ImageSubtitle>
+	</Card>
 );
 
 Post.propTypes = {
-  title: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  url: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
-  time: PropTypes.number.isRequired,
+	title: PropTypes.string.isRequired,
+	text: PropTypes.string.isRequired,
+	image: PropTypes.string.isRequired,
+	url: PropTypes.string.isRequired,
+	date: PropTypes.string.isRequired,
+	time: PropTypes.number.isRequired,
 };
 
 const parsePost = postFromGraphql => {
-  const MEDIUM_CDN = 'https://cdn-images-1.medium.com/max/400';
-  const MEDIUM_URL = 'https://medium.com';
-  const {
-    id,
-    uniqueSlug,
-    createdAt,
-    title,
-    virtuals,
-    author,
-  } = postFromGraphql;
-  const image =
-    virtuals.previewImage.imageId &&
-    `${MEDIUM_CDN}/${virtuals.previewImage.imageId}`;
-  return {
-    id,
-    title,
-    time: virtuals.readingTime,
-    date: createdAt,
-    text: virtuals.subtitle,
-    image,
-    url: `${MEDIUM_URL}/${author.username}/${uniqueSlug}`,
-  };
+	const MEDIUM_CDN = 'https://cdn-images-1.medium.com/max/400';
+	const MEDIUM_URL = 'https://medium.com';
+	const {
+		id,
+		uniqueSlug,
+		createdAt,
+		title,
+		virtuals,
+		author,
+	} = postFromGraphql;
+	const image =
+		virtuals.previewImage.imageId &&
+		`${MEDIUM_CDN}/${virtuals.previewImage.imageId}`;
+	return {
+		id,
+		title,
+		time: virtuals.readingTime,
+		date: createdAt,
+		text: virtuals.subtitle,
+		image,
+		url: `${MEDIUM_URL}/${author.username}/${uniqueSlug}`,
+	};
 };
 
 const edgeToArray = data => data.edges.map(edge => edge.node);
 
 const Writing = () => (
-  <StaticQuery
-    query={graphql`
+	<StaticQuery
+		query={graphql`
       query MediumPostQuery {
         isMediumUserDefine: __type(
           name: "contentfulAboutMediumUserQueryString_2"
@@ -129,24 +129,24 @@ const Writing = () => (
         }
       }
     `}
-    render={({ allMediumPost, isMediumUserDefine }) => {
-      const posts = edgeToArray(allMediumPost).map(parsePost);
-      return (
-        isMediumUserDefine && (
-          <Section.Container id="writing" Background={Background}>
-            <Section.Header name="Writing" icon="✍️" label="writing" />
-            <CardContainer minWidth="300px">
-              {posts.map(p => (
-                <Fade bottom>
-                  <Post key={p.id} {...p} />
-                </Fade>
-              ))}
-            </CardContainer>
-          </Section.Container>
-        )
-      );
-    }}
-  />
+		render={({ allMediumPost, isMediumUserDefine }) => {
+			const posts = edgeToArray(allMediumPost).map(parsePost);
+			return (
+				isMediumUserDefine && (
+					<Section.Container id="writing" Background={Background}>
+						<Section.Header name="Writing" icon="✍️" label="writing" />
+						<CardContainer minWidth="300px">
+							{posts.map(p => (
+								<Fade bottom>
+									<Post key={p.id} {...p} />
+								</Fade>
+							))}
+						</CardContainer>
+					</Section.Container>
+				)
+			);
+		}}
+	/>
 );
 
 export default Writing;
