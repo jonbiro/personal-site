b103f039db26aa30e069bbf830a01d402c54cb19
Jonathan Biro <jonnybrx@gmail.com>
2021-04-26 20:27:05 +0000

    blah

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,6 @@ const { ACCESS_TOKEN, SPACE_ID } = process.env;
 
 const plugins = [
   {
-    resolve: `gatsby-theme-mate`,
     options: {
       accessToken: ACCESS_TOKEN,
       spaceId: SPACE_ID,
